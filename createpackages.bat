a31640aa0ac23db3576b8856b5d4a43e39d4a4ed
Jimmy Geels <jimmy.geels@topicus.nl>
2016-09-27 16:47:45 +0200

    nuspec and deps, target 4.5.2

---
+++ createpackages.bat
@@ -0,0 +1,7 @@
+nuget.exe pack ./src/AvScan.Core/AvScan.Core.csproj
+nuget.exe pack ./src/AvScan.Avast/AvScan.Avast.csproj
+nuget.exe pack ./src/AvScan.AVG/AvScan.AVG.csproj
+nuget.exe pack ./src/AvScan.EsetScanner/AvScan.EsetScanner.csproj
+nuget.exe pack ./src/AvScan.WindowsDefender/AvScan.WindowsDefender.csproj
+nuget.exe pack ./src/AvScan.Core/AvScan.Core.csproj
+pause
